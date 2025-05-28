69207f46c6f588df847d168b82efb62739133b6c
Alex Heredia <alexheredia@Alexs-MacBook-Air.local>
2025-05-28 10:38:21 -0600

    Fix: Correct start script to use index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const blogsPath = path.join(__dirname, 'blogs.json');
 const uploadsDir = 'uploads';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Configuración inicial
 if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir);
